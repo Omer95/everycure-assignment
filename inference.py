c66bc1ab243fa17c11b14b57afab5b6484b1a960
kmcd418_azu <omerfarooq.ahmed@astrazeneca.com>
2024-06-05 14:23:35 +0100

    huggingface model inference added and response prepared

---
+++ inference.py
@@ -0,0 +1,9 @@
+from transformers import pipeline
+from transformers import AutoTokenizer, AutoModelForTokenClassification
+
+tokenizer = AutoTokenizer.from_pretrained("d4data/biomedical-ner-all")
+model = AutoModelForTokenClassification.from_pretrained("d4data/biomedical-ner-all")
+pipe = pipeline("ner", model=model, tokenizer=tokenizer, aggregation_strategy="simple")
+
+def extract_entities(text):
+    return pipe(text)
--- routes.py
+++ routes.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 from io import BytesIO
 from pdfminer_utils import process_document
+from inference import extract_entities
 
 app = Flask(__name__)
 
@@ -10,8 +11,21 @@ def extract():
         file = request.files['file']
         filedata = BytesIO(file.read())
         pages = process_document(filedata, [0,1])
-        print(pages[1])
-        return 'file found', 200
+        entities = []
+        for page in pages:
+            entities += extract_entities(pages[page])
+        response = prepare_response(entities, pages[page])
+        return response, 200
     else:
         return 'file not attached', 400
     
+def prepare_response(entities, text):
+    prepared_entities = []
+    for entity in entities:
+        prepared_entity = {'entity': entity['word'], 'start': entity['start'], 'end': entity['end']}
+        context_start = entity['start'] - 70 if entity['start'] - 70 >= 0 else 0
+        context_end = entity['end'] + 70 if entity['end'] + 70 < len(text) else len(text)
+        prepared_entity['context'] = text[context_start:context_end]
+
+        prepared_entities.append(prepared_entity)
+    return prepared_entities
